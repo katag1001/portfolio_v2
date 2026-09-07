bbc217b0402b8a89acf5671d7a46606cb3153402
katag1001 <katarinag1001@gmail.com>
2026-09-07 17:02:51 +0200

    colour changes and copy change in abuut me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -15,42 +15,14 @@ export default function AboutMe() {
         <div className="about-divider" />
 
         <div className="about-text">
-          <p>Hi, I'm Katy and I’m a full stack developer 👋. 
-            I’m a lifelong learner and lover of knowledge.</p>
+          <p>Hi, I’m Katy and I’m a full stack developer.</p> 
 
-          <p>
-            After several years working in data analysis with SQL and Python,
-            I caught the coding bug (pun intended). I’ve always been someone
-            who loves getting stuck into a problem and am able to hyper-focus
-            for hours when there’s a solution to be found. I’m at my happiest
-            when I’m building, improving, and making sense of something
-            challenging.
-          </p>
-
-          <p>
-            Right now, I’m obsessed with building my wardrobe pairing app,
-            Wearable, which you can check out in my projects above. I wanted
-            to crack the secret of what makes an outfit look good and build
-            that into an algorithm. I also wanted to solve the age-old problem
-            of having a wardrobe full of clothes and nothing to wear, so that
-            when someone uses my app, they’re simply given the perfect option
-            based on the weather, their existing wardrobe, and what they
-            haven’t worn recently.
-          </p>
-
-          <p>
-            And when I’m not coding, you’ll usually find me sat at (and
-            sometimes sewn into) my sewing machine, solving problems in a
-            different way. I love the engineering behind constructing a
-            garment and how you can mathematically transform a flat piece of
-            fabric into a three-dimensional object that fits and moves with
-            the human body.
-          </p>
-
-          <p>
-            If you want to know more, feel free to get in touch below or
-            download my CV. Looking forward to hearing from you!
-          </p>
+        <p>I work primarily with the MERN stack but I’m a lifelong learner and lover of knowledge. 
+          I'm passionate about making things and fixing things. Outside of work, you’ll usually 
+          find me at (and sometimes sewn into) my sewing machine. I always need to be engineering 
+          something, whether that's a web app or a piece of clothing, I just love to figure out 
+          how things are made and solve problems. I've lived all over the world collect 
+          languages (both human and computer) like they're pokemon.</p>
 
           <a
             href="/Katarina_Grantham_CV.pdf"
